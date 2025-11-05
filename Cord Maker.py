5171ab85c1f29ee49a47c5f26019bb97d523b01e
jxndvs <jxn.dvs13@gmail.com>
2025-11-04 22:38:01 -0600

    Add files via upload

---
+++ Cord Maker.py
@@ -0,0 +1,830 @@
+import pickle
+class Cord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.CordType = ""
+    def give_stuff(self, type):
+        self.CordType = type
+    def display(self, n):
+        print(f"c{n} : ({self.x},{self.y}) : {self.CordType}")
+
+cords = []
+nx = 1
+ny = 1
+
+c1 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c1.give_stuff("#")
+cords.append(c1)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c2 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c2.give_stuff("#")
+cords.append(c2)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c3 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c3.give_stuff("#")
+cords.append(c3)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c4 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c4.give_stuff("#")
+cords.append(c4)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c5 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c5.give_stuff("#")
+cords.append(c5)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c6 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c6.give_stuff("#")
+cords.append(c6)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c7 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c7.give_stuff("#")
+cords.append(c7)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c8 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c8.give_stuff("#")
+cords.append(c8)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c9 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c9.give_stuff("#")
+cords.append(c9)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c10 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c10.give_stuff("#")
+cords.append(c10)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c11 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c11.give_stuff("#")
+cords.append(c11)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c12 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c12.give_stuff("#")
+cords.append(c12)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c13 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c13.give_stuff("#")
+cords.append(c13)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c14 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c14.give_stuff("#")
+cords.append(c14)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c15 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c15.give_stuff("#")
+cords.append(c15)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c16 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c16.give_stuff("#")
+cords.append(c16)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c17 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c17.give_stuff("#")
+cords.append(c17)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c18 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c18.give_stuff("#")
+cords.append(c18)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c19 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c19.give_stuff("#")
+cords.append(c19)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c20 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c20.give_stuff("#")
+cords.append(c20)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c21 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c21.give_stuff("#")
+cords.append(c21)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c22 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c22.give_stuff("#")
+cords.append(c22)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c23 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c23.give_stuff("#")
+cords.append(c23)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c24 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c24.give_stuff("#")
+cords.append(c24)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c25 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c25.give_stuff("#")
+cords.append(c25)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c26 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c26.give_stuff("#")
+cords.append(c26)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c27 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c27.give_stuff("#")
+cords.append(c27)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c28 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c28.give_stuff("#")
+cords.append(c28)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c29 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c29.give_stuff("#")
+cords.append(c29)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c30 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c30.give_stuff("#")
+cords.append(c30)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c31 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c31.give_stuff("#")
+cords.append(c31)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c32 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c32.give_stuff("#")
+cords.append(c32)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c33 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c33.give_stuff("#")
+cords.append(c33)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c34 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c34.give_stuff("#")
+cords.append(c34)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c35 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c35.give_stuff("#")
+cords.append(c35)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c36 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c36.give_stuff("#")
+cords.append(c36)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c37 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c37.give_stuff("#")
+cords.append(c37)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c38 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c38.give_stuff("#")
+cords.append(c38)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c39 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c39.give_stuff("#")
+cords.append(c39)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c40 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c40.give_stuff("#")
+cords.append(c40)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c41 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c41.give_stuff("#")
+cords.append(c41)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c42 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c42.give_stuff("#")
+cords.append(c42)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c43 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c43.give_stuff("#")
+cords.append(c43)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c44 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c44.give_stuff("#")
+cords.append(c44)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c45 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c45.give_stuff("#")
+cords.append(c45)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c46 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c46.give_stuff("#")
+cords.append(c46)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c47 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c47.give_stuff("#")
+cords.append(c47)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c48 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c48.give_stuff("#")
+cords.append(c48)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c49 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c49.give_stuff("#")
+cords.append(c49)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c50 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c50.give_stuff("#")
+cords.append(c50)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c51 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c51.give_stuff("#")
+cords.append(c51)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c52 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c52.give_stuff("#")
+cords.append(c52)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c53 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c53.give_stuff("#")
+cords.append(c53)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c54 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c54.give_stuff("#")
+cords.append(c54)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c55 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c55.give_stuff("#")
+cords.append(c55)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c56 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c56.give_stuff("#")
+cords.append(c56)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c57 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c57.give_stuff("#")
+cords.append(c57)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c58 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c58.give_stuff("#")
+cords.append(c58)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c59 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c59.give_stuff("#")
+cords.append(c59)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c60 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c60.give_stuff("#")
+cords.append(c60)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c61 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c61.give_stuff("#")
+cords.append(c61)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c62 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c62.give_stuff("#")
+cords.append(c62)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c63 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c63.give_stuff("#")
+cords.append(c63)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c64 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c64.give_stuff("#")
+cords.append(c64)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c65 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c65.give_stuff("#")
+cords.append(c65)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c66 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c66.give_stuff("#")
+cords.append(c66)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c67 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c67.give_stuff("#")
+cords.append(c67)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c68 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c68.give_stuff("#")
+cords.append(c68)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c69 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c69.give_stuff("#")
+cords.append(c69)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+c70 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c70.give_stuff("#")
+cords.append(c70)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c71 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c71.give_stuff("#")
+cords.append(c71)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c72 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c72.give_stuff("#")
+cords.append(c72)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c73 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c73.give_stuff("#")
+cords.append(c73)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c74 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c74.give_stuff("#")
+cords.append(c74)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c75 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c75.give_stuff("#")
+cords.append(c75)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c76 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c76.give_stuff("#")
+cords.append(c76)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c77 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c77.give_stuff("#")
+cords.append(c77)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c78 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c78.give_stuff("#")
+cords.append(c78)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c79 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c79.give_stuff("#")
+cords.append(c79)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+c80 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c80.give_stuff("#")
+cords.append(c80)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+c81 = Cord(nx, ny)
+if nx == 1 or nx == 9 or ny == 1 or ny == 9:
+    c81.give_stuff("#")
+cords.append(c81)
+if nx == 9:
+    nx = 1
+    ny += 1
+else:
+    nx += 1
+
+n = 1
+for x in cords:
+    x.display(n)
+    n+=1
+
+CordDat = open("coordinates.dat", "wb")
+pickle.dump(cords, CordDat)
