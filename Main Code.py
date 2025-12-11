c40fea209e22faf68afc6f56155b66a9f867f243
jxndvs <jxn.dvs13@gmail.com>
2025-12-11 12:06:57 -0600

    Shop System *ALMOST* Done

--- Main Code.py
+++ Main Code.py
@@ -3,7 +3,7 @@ import random
 import tkinter as tk
 from tkinter import *
 screen = Tk()
-screen.geometry("720x500")
+screen.geometry("703x490")
 screen.resizable(False, False)
 
 class Cord:
@@ -19,25 +19,43 @@ class Player:
         self.y = 5
         self.MaxHP = 3
         self.HP = 3
+        self.PermHP = 0
         self.Dmg = 1
         self.Res = 0
-        self.Weapon = "Ranged"
+        self.Weapon = "Normal"
         self.Spd = 1
-        self.Bank = 0
+        self.Bank = 1
         self.Symbol = "0"
-    def IncMaxHP(self, n):
-        self.MaxHP += n
+    def NewMaxHP(self, n):
+        o = self.MaxHP
+        self.MaxHP = n
+        if n != o:
+            self.EditHealth(n-o)
+            if P.HP == 0:
+                P.HP = 1
+    def EditHealth(self,n):
+        self.HP += n
+    def Heal(self,n):
+        self.HP += n
+        if self.HP > self.MaxHP:
+            self.HP = self.MaxHP
+    def IncHP(self,n):
+        self.PermHP += n
+        self.EditHealth(n)
     def NewDmg(self, n):
         self.Dmg = n
     def NewWeapon(self, x):
-        self.Weapon = X
+        self.Weapon = x
+    def NewSpeed(self, x):
+        self.Spd = x
     def Hurt(self, dmg):
         ndmg = dmg - self.Res
         self.HP -= ndmg
-        if self.HP <= self.MaxHP/3*2:
+        if self.HP < self.MaxHP/3*2+1:
             self.Symbol = "O"
-        if self.HP <= self.MaxHP/3:
+        if self.HP < self.MaxHP/3+1:
             self.Symbol = "o"
+        f_cord(self.x, self.y).give_stuff(self.Symbol)
     def MoveUp(self):
         global Moves
         if f_cord(self.x,self.y-1) and Moves>0:
@@ -120,11 +138,13 @@ class Player:
                     f_enemy(P.x, P.y - 1).Hurt(self.Dmg)
                     Attacks -= 1
                 elif P.Weapon == "Ranged":
+                    still = "Y"
                     for r in range (2,8):
-                        if f_cord(P.x,P.y-r):
+                        if f_cord(P.x,P.y-r) and still == "Y":
                             if f_cord(P.x,P.y-r).CordType != " ":
                                 f_enemy(P.x,P.y-r).Hurt(self.Dmg)
                                 Attacks -= 1
+                                still = "N"
             if self.Weapon == "Cleave":
                 C1 = f_cord(P.x+1,P.y-1)
                 if C1.CordType:
@@ -146,11 +166,13 @@ class Player:
                     f_enemy(P.x - 1, P.y).Hurt(self.Dmg)
                     Attacks -= 1
                 elif P.Weapon == "Ranged":
+                    still = "Y"
                     for r in range(2, 8):
-                        if f_cord(P.x-r, P.y):
+                        if f_cord(P.x-r, P.y) and still == "Y":
                             if f_cord(P.x-r, P.y).CordType != " ":
                                 f_enemy(P.x-r, P.y).Hurt(self.Dmg)
                                 Attacks -= 1
+                                still = "N"
             if self.Weapon == "Cleave":
                 C1 = f_cord(P.x - 1, P.y - 1)
                 if C1.CordType:
@@ -172,11 +194,13 @@ class Player:
                     f_enemy(P.x,P.y+1).Hurt(self.Dmg)
                     Attacks -= 1
                 elif P.Weapon == "Ranged":
+                    still = "Y"
                     for r in range(2, 8):
-                        if f_cord(P.x, P.y+r):
+                        if f_cord(P.x, P.y+r) and still == "Y":
                             if f_cord(P.x, P.y+r).CordType != " ":
                                 f_enemy(P.x, P.y+r).Hurt(self.Dmg)
                                 Attacks -= 1
+                                still = "N"
             if self.Weapon == "Cleave":
                 C1 = f_cord(P.x+1,P.y+1)
                 if C1.CordType:
@@ -198,11 +222,13 @@ class Player:
                     f_enemy(P.x + 1, P.y).Hurt(self.Dmg)
                     Attacks -= 1
                 elif P.Weapon == "Ranged":
+                    still = "Y"
                     for r in range(2, 8):
-                        if f_cord(P.x+r, P.y):
+                        if f_cord(P.x+r, P.y) and still == "Y":
                             if f_cord(P.x+r, P.y).CordType != " ":
                                 f_enemy(P.x+r, P.y).Hurt(self.Dmg)
                                 Attacks -= 1
+                                still = "N"
             if self.Weapon == "Cleave":
                 C1 = f_cord(P.x + 1, P.y - 1)
                 if C1.CordType:
@@ -220,15 +246,66 @@ class Player:
         if Attacks > 0:
             Attacks -= 1
             RunTurn()
-class Weapon:
-    def __init__(self):
-        self.Weapon_Name = ""
-        self.Weapon_Cost = ""
-        self.Weapon_Damage = ""
-        self.Weapon_Type = ""
+class Purchasable:
+    def __init__(self,name,cost,rare):
+        self.Name= name
+        self.Cost = cost
+        self.Rarity = rare
+        global ShopC
+        global ShopR
+        global ShopL
+        if rare==1:
+            ShopC.append(self)
+        elif rare==2:
+            ShopR.append(self)
+        elif rare==3:
+            ShopL.append(self)
+class Weapon(Purchasable):
+    def __init__(self,name,cost,rarity,dmg,type):
+        Purchasable.__init__(self,name,cost,rarity)
+        self.Damage = dmg
+        self.Type = type
     def Equip(self):
-        P.NewDmg(self.Weapon_Damage)
-        P.NewWeapon(self.Weapon_Type)
+        P.NewDmg(self.Damage)
+        P.NewWeapon(self.Type)
+        print("Equipped")
+    def Display(self):
+        return f"{self.Name}\n{self.Type}\nDmg: {self.Damage}\nCost: {self.Cost}"
+class Armor(Purchasable):
+    def __init__(self,name,cost,rarity,hp,res):
+        Purchasable.__init__(self,name,cost,rarity)
+        self.MaxHP = hp
+        self.Resistance = res
+    def Equip(self):
+        P.NewMaxHP(self.MaxHP+P.PermHP)
+        P.NewWeapon(self.Resistance)
+    def Display(self):
+        return f"{self.Name}\nMax HP: {self.MaxHP}\nRes: {self.Resistance}\nCost: {self.Cost}"
+class Artifact(Purchasable):
+    def __init__(self,name,cost,rarity,spd,hp):
+        Purchasable.__init__(self,name,cost,rarity)
+        self.Speed = spd
+        self.HP = hp
+    def Equip(self):
+        P.IncHP(self.HP)
+        P.NewSpeed(self.Speed)
+    def Display(self):
+        return f"{self.Name}\nHP Up: {self.HP}\nSpd: {self.Speed}\nCost: {self.Cost}"
+class Item(Purchasable):
+    def __init__(self,name,cost,rarity,effect,hp,move,atk):
+        Purchasable.__init__(self,name,cost,rarity)
+        self.Text = effect
+        self.Heal = hp
+        self.Dash = move
+        self.Haste = atk
+    def Equip(self):
+        global Moves
+        Moves += self.Dash
+        global Attacks
+        Attacks += self.Haste
+        P.Heal(self.Heal)
+    def Display(self):
+        return f"{self.Name}\n{self.Text}\nCost: {self.Cost}"
 class Enemy:
     def __init__(self,x,y,Smbl,Hp,Dmg,Spd,Wv,Vlu):
         self.x = x
@@ -291,7 +368,7 @@ class Enemy:
         elif self.x < pX and CR.CordType == " ":
             self.x += 1
             Co.give_stuff(" ")
-        f_cord(self.x, self.y).give_stuff("X")
+        f_cord(self.x, self.y).give_stuff(self.Symbol)
 class Thug(Enemy):
     def __init__(self,x,y,Wv):
         Enemy.__init__(self,x,y,"T",1,1,1,Wv,1)
@@ -315,6 +392,31 @@ class Thug(Enemy):
             Co = f_cord(self.x, self.y)
             Co.give_stuff(" ")
             P.Bank += self.Value
+class Squire(Enemy):
+    def __init__(self,x,y,Wv):
+        Enemy.__init__(self,x,y,"S",2,2,1,Wv,2)
+    def Attack(self):
+        if f_cord(self.x, self.y - 1):
+            if f_cord(self.x, self.y - 1).CordType == P.Symbol:
+                P.Hurt(self.EDmg)
+        if f_cord(self.x - 1, self.y):
+            if f_cord(self.x - 1, self.y).CordType == P.Symbol:
+                P.Hurt(self.EDmg)
+        if f_cord(self.x, self.y + 1):
+            if f_cord(self.x, self.y + 1).CordType == P.Symbol:
+                P.Hurt(self.EDmg)
+        if f_cord(self.x + 1, self.y):
+            if f_cord(self.x + 1, self.y).CordType == P.Symbol:
+                P.Hurt(self.EDmg)
+    def Hurt(self, dmg):
+        self.EHp -= dmg
+        if self.EHp < 1:
+            activeEs.remove(self)
+            Co = f_cord(self.x, self.y)
+            Co.give_stuff(" ")
+            P.Bank += self.Value
+        if self.EHp == 1:
+            self.nSymbol("s")
 def f_cord(fx, fy):
     for find in cords:
         if find.x == fx and find.y == fy:
@@ -357,26 +459,26 @@ def PrintScreen():
     if P.HP > 0:
         Display.config(state=NORMAL)
         Display.delete(1.0, "end-1c")
-        Display.insert(tk.INSERT, f"# # # # # # # # # # #\n")
+        Display.insert(tk.INSERT, f"# # # # # # # # # # #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 1).CordType} {f_cord(2, 1).CordType} {f_cord(3, 1).CordType} {f_cord(4, 1).CordType} {f_cord(5, 1).CordType} {f_cord(6, 1).CordType} {f_cord(7, 1).CordType} {f_cord(8, 1).CordType} {f_cord(9, 1).CordType} #\n")
+                       f"# {f_cord(1, 1).CordType} {f_cord(2, 1).CordType} {f_cord(3, 1).CordType} {f_cord(4, 1).CordType} {f_cord(5, 1).CordType} {f_cord(6, 1).CordType} {f_cord(7, 1).CordType} {f_cord(8, 1).CordType} {f_cord(9, 1).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 2).CordType} {f_cord(2, 2).CordType} {f_cord(3, 2).CordType} {f_cord(4, 2).CordType} {f_cord(5, 2).CordType} {f_cord(6, 2).CordType} {f_cord(7, 2).CordType} {f_cord(8, 2).CordType} {f_cord(9, 2).CordType} #\n")
+                       f"# {f_cord(1, 2).CordType} {f_cord(2, 2).CordType} {f_cord(3, 2).CordType} {f_cord(4, 2).CordType} {f_cord(5, 2).CordType} {f_cord(6, 2).CordType} {f_cord(7, 2).CordType} {f_cord(8, 2).CordType} {f_cord(9, 2).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 3).CordType} {f_cord(2, 3).CordType} {f_cord(3, 3).CordType} {f_cord(4, 3).CordType} {f_cord(5, 3).CordType} {f_cord(6, 3).CordType} {f_cord(7, 3).CordType} {f_cord(8, 3).CordType} {f_cord(9, 3).CordType} #\n")
+                       f"# {f_cord(1, 3).CordType} {f_cord(2, 3).CordType} {f_cord(3, 3).CordType} {f_cord(4, 3).CordType} {f_cord(5, 3).CordType} {f_cord(6, 3).CordType} {f_cord(7, 3).CordType} {f_cord(8, 3).CordType} {f_cord(9, 3).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 4).CordType} {f_cord(2, 4).CordType} {f_cord(3, 4).CordType} {f_cord(4, 4).CordType} {f_cord(5, 4).CordType} {f_cord(6, 4).CordType} {f_cord(7, 4).CordType} {f_cord(8, 4).CordType} {f_cord(9, 4).CordType} #\n")
+                       f"# {f_cord(1, 4).CordType} {f_cord(2, 4).CordType} {f_cord(3, 4).CordType} {f_cord(4, 4).CordType} {f_cord(5, 4).CordType} {f_cord(6, 4).CordType} {f_cord(7, 4).CordType} {f_cord(8, 4).CordType} {f_cord(9, 4).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 5).CordType} {f_cord(2, 5).CordType} {f_cord(3, 5).CordType} {f_cord(4, 5).CordType} {f_cord(5, 5).CordType} {f_cord(6, 5).CordType} {f_cord(7, 5).CordType} {f_cord(8, 5).CordType} {f_cord(9, 5).CordType} #\n")
+                       f"# {f_cord(1, 5).CordType} {f_cord(2, 5).CordType} {f_cord(3, 5).CordType} {f_cord(4, 5).CordType} {f_cord(5, 5).CordType} {f_cord(6, 5).CordType} {f_cord(7, 5).CordType} {f_cord(8, 5).CordType} {f_cord(9, 5).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 6).CordType} {f_cord(2, 6).CordType} {f_cord(3, 6).CordType} {f_cord(4, 6).CordType} {f_cord(5, 6).CordType} {f_cord(6, 6).CordType} {f_cord(7, 6).CordType} {f_cord(8, 6).CordType} {f_cord(9, 6).CordType} #\n")
+                       f"# {f_cord(1, 6).CordType} {f_cord(2, 6).CordType} {f_cord(3, 6).CordType} {f_cord(4, 6).CordType} {f_cord(5, 6).CordType} {f_cord(6, 6).CordType} {f_cord(7, 6).CordType} {f_cord(8, 6).CordType} {f_cord(9, 6).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 7).CordType} {f_cord(2, 7).CordType} {f_cord(3, 7).CordType} {f_cord(4, 7).CordType} {f_cord(5, 7).CordType} {f_cord(6, 7).CordType} {f_cord(7, 7).CordType} {f_cord(8, 7).CordType} {f_cord(9, 7).CordType} #\n")
+                       f"# {f_cord(1, 7).CordType} {f_cord(2, 7).CordType} {f_cord(3, 7).CordType} {f_cord(4, 7).CordType} {f_cord(5, 7).CordType} {f_cord(6, 7).CordType} {f_cord(7, 7).CordType} {f_cord(8, 7).CordType} {f_cord(9, 7).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 8).CordType} {f_cord(2, 8).CordType} {f_cord(3, 8).CordType} {f_cord(4, 8).CordType} {f_cord(5, 8).CordType} {f_cord(6, 8).CordType} {f_cord(7, 8).CordType} {f_cord(8, 8).CordType} {f_cord(9, 8).CordType} #\n")
+                       f"# {f_cord(1, 8).CordType} {f_cord(2, 8).CordType} {f_cord(3, 8).CordType} {f_cord(4, 8).CordType} {f_cord(5, 8).CordType} {f_cord(6, 8).CordType} {f_cord(7, 8).CordType} {f_cord(8, 8).CordType} {f_cord(9, 8).CordType} #\n", ("display_tag",))
         Display.insert(tk.INSERT,
-                       f"# {f_cord(1, 9).CordType} {f_cord(2, 9).CordType} {f_cord(3, 9).CordType} {f_cord(4, 9).CordType} {f_cord(5, 9).CordType} {f_cord(6, 9).CordType} {f_cord(7, 9).CordType} {f_cord(8, 9).CordType} {f_cord(9, 9).CordType} #\n")
-        Display.insert(tk.INSERT, f"# # # # # # # # # # #")
+                       f"# {f_cord(1, 9).CordType} {f_cord(2, 9).CordType} {f_cord(3, 9).CordType} {f_cord(4, 9).CordType} {f_cord(5, 9).CordType} {f_cord(6, 9).CordType} {f_cord(7, 9).CordType} {f_cord(8, 9).CordType} {f_cord(9, 9).CordType} #\n", ("display_tag",))
+        Display.insert(tk.INSERT, f"# # # # # # # # # # #", ("display_tag",))
         Display.config(state=DISABLED)
         Health.config(state=NORMAL)
         Health.delete(1.0, "end-1c")
@@ -418,6 +520,63 @@ def PrintScreen():
         Display.config(state=NORMAL)
         Display.delete(1.0, "end-1c")
         Display.insert(tk.INSERT, f"\n\n\n\n\n      Game Over")
+def RefreshShop():
+    if P.Bank > 0:
+        P.Bank -= 1
+        global ShopC
+        global ShopR
+        global ShopL
+        global Product1
+        P1 = random.randint(0, len(ShopC) - 1)
+        Product1 = ShopC[P1]
+        Shop1.config(state=NORMAL)
+        Shop1.delete(1.0, "end-1c")
+        Shop1.insert(tk.INSERT, Product1.Display())
+        Shop1.config(state=DISABLED)
+        global Product2
+        R2 = random.randint(1,WaveN + 5)
+        if R2 < 6:
+            P2 = random.randint(0, len(ShopC) - 1)
+            Product2 = ShopC[P2]
+        if R2 > 5:
+            P2 = random.randint(0, len(ShopR) - 1)
+            Product2 = ShopC[P2]
+        Shop2.config(state=NORMAL)
+        Shop2.delete(1.0, "end-1c")
+        Shop2.insert(tk.INSERT, Product1.Display())
+        Shop2.config(state=DISABLED)
+        global Product3
+        R3 = random.randint(1,int(WaveN * 1.2 + 3))
+        if R3 < 4:
+            P3 = random.randint(0, len(ShopC) - 1)
+            Product2 = ShopC[P3]
+        if R3 > 3 and R3 < int(3 + WaveN * 0.7 + 3):
+            P3 = random.randint(0, len(ShopR) - 1)
+            Product2 = ShopC[P3]
+        if R3 > (3 + WaveN * 0.7 + 2.99):
+            P3 = random.randint(0, len(ShopR) - 1)
+            Product2 = ShopC[P3]
+        Shop3.config(state=NORMAL)
+        Shop3.delete(1.0, "end-1c")
+        Shop3.insert(tk.INSERT, Product1.Display())
+        Shop3.config(state=DISABLED)
+        print(R2)
+        print(R3)
+def Buy1():
+    if P.Bank >= Product1.Cost:
+        Product1.Equip()
+        P.Bank -= Product1.Cost
+        PrintScreen()
+def Buy2():
+    if P.Bank >= Product2.Cost:
+        Product1.Equip()
+        P.Bank -= Product2.Cost
+        PrintScreen()
+def Buy3():
+    if P.Bank >= Product3.Cost:
+        Product1.Equip()
+        P.Bank -= Product3.Cost
+        PrintScreen()
 
 with open("coordinates.dat", "rb") as cdat:
     cords = pickle.load(cdat)
@@ -426,19 +585,27 @@ C = f_cord(P.x,P.y)
 C.give_stuff(P.Symbol)
 activeEs = []
 allEs=[]
+ShopC=[]
+ShopR=[]
+ShopL=[]
+Product1 = None
+Product2 = None
+Product3 = None
+w = Weapon("Battle Axe",1,1,2,"Cleave")
 e = Thug(9,5,1)
 e = Thug(9,1,2)
 e = Thug(1,9,2)
 e = Thug(1,2,3)
 e = Thug(6,1,3)
-e = Thug(8,9,3)
+e = Squire(8,9,3)
 Moves = P.Spd
 Attacks = 1
 WaveN = 0
 WaveStart()
 
-Display = Text(screen,width=21,height=11,font=("fixedsys",21))
+Display = Text(screen,width=22,height=11,font=("fixedsys",21))
 Display.place(x=10,y=10)
+Display.tag_configure("display_tag", justify="center")
 MUp = Button(screen,width=3, height=1,font=("fixedsys",21),text="↑",command=P.MoveUp)
 MUp.place(x=440,y=120)
 MLeft = Button(screen,width=3, height=1,font=("fixedsys",21),text="←",command=P.MoveLeft)
@@ -453,17 +620,17 @@ MLabel = Text(screen,width=7,height=2,font=("fixedsys",16))
 MLabel.place(x=373,y=129)
 MLabel.tag_configure("center_tag", justify="center")
 AUp = Button(screen,width=3, height=1,font=("fixedsys",21),text="↑",command=P.AttackUp)
-AUp.place(x=440,y=310)
+AUp.place(x=570,y=240)
 ALeft = Button(screen,width=3, height=1,font=("fixedsys",21),text="←",command=P.AttackLeft)
-ALeft.place(x=375,y=370)
+ALeft.place(x=505,y=300)
 ADown = Button(screen,width=3, height=1,font=("fixedsys",21),text="↓",command=P.AttackDown)
-ADown.place(x=440,y=430)
+ADown.place(x=570,y=360)
 ARight = Button(screen,width=3, height=1,font=("fixedsys",21),text="→",command=P.AttackRight)
-ARight.place(x=505,y=370)
+ARight.place(x=635,y=300)
 ASkip = Button(screen,width=6, height=3,font=("fixedsys",16),text="Skip",command=P.AttackSkip)
-ASkip.place(x=440,y=369)
+ASkip.place(x=570,y=299)
 ALabel = Text(screen,width=7,height=2,font=("fixedsys",16))
-ALabel.place(x=373,y=319)
+ALabel.place(x=633,y=249)
 ALabel.tag_configure("center_tag", justify="center")
 Health = Text(screen,width=9,height=1,font=("fixedsys",21))
 Health.place(x=375,y=10)
@@ -479,8 +646,21 @@ Bank = Text(screen,width=9,height=1,font=("fixedsys",21))
 Bank.place(x=523,y=80)
 WaveCounter = Text(screen,width=6,height=1,font=("fixedsys",21))
 WaveCounter.place(x=571,y=115)
-Shop1 = Text(screen,width=12,height=4,font=("fixedsys",21))
-Shop1 =
+Shop1 = Text(screen,width=10,height=4,font=("fixedsys",21))
+Shop1.place(x=10,y=356)
+Buy1 = Button(screen,width=2,height=1,font=("fixedsys",16),text="$",command=Buy1)
+Buy1.place(x=142,y=450)
+Shop2 = Text(screen,width=10,height=4,font=("fixedsys",21))
+Shop2.place(x=172,y=356)
+Buy2 = Button(screen,width=2,height=1,font=("fixedsys",16),text="$",command=Buy2)
+Buy2.place(x=304,y=450)
+Shop3 = Text(screen,width=10,height=4,font=("fixedsys",21))
+Shop3.place(x=334,y=356)
+Buy3 = Button(screen,width=2,height=1,font=("fixedsys",16),text="$",command=Buy3)
+Buy3.place(x=466,y=450)
+ReShop = Button(screen,width=14,height=1,font=("fixedsys",16),text="$Refresh Shop$",command=RefreshShop)
+ReShop.place(x=375,y=318)
 
+RefreshShop()
 PrintScreen()
 screen.mainloop()
